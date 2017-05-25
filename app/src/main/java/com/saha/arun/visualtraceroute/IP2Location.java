e3c967442dc8eb05219a55cf39194fbd929ace17
Arun Kumar Shreevastava <arunkr9088@gmail.com>
2017-05-25 21:51:24 +0530

    ip2location init commit

---
+++ app/src/main/java/com/saha/arun/visualtraceroute/IP2Location.java
@@ -0,0 +1,38 @@
+package com.saha.arun.visualtraceroute;
+
+import java.util.StringTokenizer;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by arunkr on 25/5/17.
+ */
+
+public class IP2Location
+{
+    private static IpAPI service;
+
+    public static Call<IPLocation> getLocation(String ip)
+    {
+        if(service==null)
+        {
+            OkHttpClient.Builder httpClient = new OkHttpClient.Builder()
+                    .readTimeout(2, TimeUnit.MINUTES)
+                    .writeTimeout(2, TimeUnit.MINUTES)
+                    .connectTimeout(3, TimeUnit.MINUTES);
+
+            Retrofit retrofit = new Retrofit.Builder()
+                    .client(httpClient.build())
+                    .baseUrl("http://ip-api.com/json")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
+            service = retrofit.create(IpAPI.class);
+        }
+        return service.getLocation(ip);
+    }
+}
---
+++ app/src/main/java/com/saha/arun/visualtraceroute/IpAPI.java
@@ -0,0 +1,15 @@
+package com.saha.arun.visualtraceroute;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+/**
+ * Created by arunkr on 25/5/17.
+ */
+
+public interface IpAPI
+{
+    @GET("/{ip}")
+    Call<IPLocation> getLocation(@Path("ip") String ip);
+}
